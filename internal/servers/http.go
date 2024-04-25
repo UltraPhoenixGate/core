fd894452362809970ded9924b4880c88a840be01
wearzdk <hi@wearzdk.me>
2024-04-25 21:36:29 +0800

    feat: add http support

---
+++ internal/servers/http.go
@@ -0,0 +1,43 @@
+package servers
+
+import (
+	"io"
+	"net/http"
+	"ultraphx-core/internal/hub"
+
+	"github.com/sirupsen/logrus"
+)
+
+func httpBroadcastHandler(w http.ResponseWriter, r *http.Request, h *hub.Hub) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		logrus.WithError(err).Error("Failed to read request body")
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		return
+	}
+
+	msg, err := hub.PraseMessageByte(body)
+	if err != nil {
+		logrus.WithError(err).Error("Failed to parse message")
+		http.Error(w, "Failed to parse message", http.StatusBadRequest)
+		return
+	}
+
+	h.Broadcast(msg)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
+}
+
+func ServeHttp(h *hub.Hub) {
+	httpMap := http.NewServeMux()
+	httpMap.HandleFunc("/broadcast", func(w http.ResponseWriter, r *http.Request) {
+		httpBroadcastHandler(w, r, h) // Pass the hub to the httpBroadcastHandler function
+	})
+	logrus.Info("Starting HTTP server on :8081")
+	http.ListenAndServe(":8081", httpMap)
+}
